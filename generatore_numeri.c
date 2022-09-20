04ddf41849370bff0d7c1283d59a59112a0badaa
Andrea Marchetto <72484739+andreamarchetto04@users.noreply.github.com>
2022-09-20 20:57:31 +0200

    Add files via upload

---
+++ generatore_numeri.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define DIM 1000000
+
+int main()
+{
+    int numeri[DIM], numero = 0;
+    FILE *file;
+    srand(time(NULL));
+    for (int i = 0; i < DIM; i++)
+    {
+        numero = rand() % DIM;
+        for (int j = 0; j < i; j++)
+        {
+            if (numero == numeri[j])
+            {
+                i--;
+                break;
+            }
+            if (j == i - 1)
+            {
+                numeri[i] = numero;
+                fprintf(file, "%d\n", numeri[i]);
+            }
+        }
+
+    }
+
+    fclose(file);
+    return 0;
+}
